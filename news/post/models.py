c2a6bb795853aded9c2fb2f0f755ba7fa01ed3ca
Rahul Kumar Gupta <rr916122@gmail.com>
2018-09-10 14:43:08 +0530

    last update of monday 10/08/2018

--- news/post/models.py
+++ news/post/models.py
@@ -14,12 +14,21 @@ class Post(models.Model):
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
     title=models.CharField(max_length=500,blank=True)
     post = models.TextField(blank=True)
+    post_para_2 = models.TextField(blank=True)
+    post_para_3 = models.TextField(blank=True)
     captions=models.TextField(blank=True)
+    captions_para_2 = models.TextField(blank=True)
+    captions_para_3 = models.TextField(blank=True)
     quote=models.CharField(max_length=200,blank=True)
     quote_by=models.CharField(max_length=100,blank=True)
     image=models.ImageField(upload_to='post_image/',blank=True)
     date= models.DateTimeField(default=now)
-
+    related_post_1 = models.CharField(max_length=100,blank=True)
+    related_link_1 = models.CharField(max_length=100,blank=True)
+    related_post_2 = models.CharField(max_length=100,blank=True)
+    related_link_2 = models.CharField(max_length=100,blank=True)
+    related_post_3 = models.CharField(max_length=100,blank=True)
+    related_link_3 = models.CharField(max_length=100,blank=True)
     def __str__(self):
         return self.title
 
